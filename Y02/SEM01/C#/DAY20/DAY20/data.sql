577ab69a47d4982dcef093671bad97a4655591c4
Naveen.mn <naveenm2795@gmail.com>
2025-11-16 06:54:28 +0530

    11_16_c#

---
+++ Y02/SEM01/C#/DAY20/DAY20/data.sql
@@ -0,0 +1,5 @@
+create table data_user(
+	user int primary key auto_increment,
+	name varchar(50),
+	password varchar(15)
+);
---
+++ Y02/SEM01/C#/DAY20/DAY20/loginForm/loginForm/DataConn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace loginForm
+{
+    String sqlconn = "Server=Localhost;Databse=userlogin;User Id=root";
+    internal class DataConn
+    {
+    }
+}
